895e18a1bfebd345aeaca14c4ab6c0ec98057f63
Shubham Dash <shubham.ddash@gmail.com>
2018-06-12 15:35:36 -0400

    second commit

--- AE.py
+++ AE.py
@@ -4,6 +4,7 @@ Created on Mon Jun 11 23:26:12 2018
 Python implementation of AutoEncoders in tensorflow
 @author: Shubham
 """
+# AutoEncoders applied on R*1 vector, Dense networks used
 
 import numpy as np
 import os
@@ -11,12 +12,46 @@ import sys
 
 # import tensorflow libraries
 import tensorflow as tf
+import tensorflow.contrib.layers as layers
 
+# Class containing AutoEncoder architecture
 class AE():
     
-    def __init__(self):
-        pass
-    
+    # parameter intialization
+    def __init__(self, lr=0.001, epochs=50, batch_size=50,
+                 is_training=True, num_nodes=1024, **kwargs):
+        self.lr = lr
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.is_training = is_training
+        self.num_nodes = num_nodes
+        
+    # Dense encoder architecture
     def buildEncoder(self, x):
+        net = layers.fully_connected(inputs=x, self.num_nodes,
+                                     activation=tf.nn.relu, scope='Dense_Enc1')
+        net = layers.fully_connected(inputs=net, self.num_nodes/2,
+                                     activation=tf.nn.relu, scope='Dense_Enc2')
+        net = layers.fully_connected(inputs=net, self.num_nodes/4,
+                                     activation=tf.nn.relu, scope='Dense_Enc3')
+        return net
+
+    # Dense Decoder architecture
+    def buildEncoder(self, enc_x):
+        net = layers.fully_connected(inputs=enc_x, self.num_nodes/4,
+                                     activation=tf.nn.relu, scope='Dense_Dec1')
+        net = layers.fully_connected(inputs=net, self.num_nodes/2,
+                                     activation=tf.nn.relu, scope='Dense_Dec2')
+        net = layers.fully_connected(inputs=net, self.num_nodes,
+                                     activation=tf.nn.relu, scope='Dense_Dec3')
+        return net
+    
+    def train(self):
+        sess = tf.Session()
+        
+        
+    
+    
+        
         
         
--- decision.py
+++ decision.py
@@ -9,6 +9,7 @@ Created on Tue Jun 12 14:22:14 2018
 import numpy as np
 import sys
 import os
+import numpy.linalg as LA
 from sklearn import utils, svm, metrics
 import matplotlib.pyplot as plt
 
@@ -18,13 +19,31 @@ from model import *
 from squeezenet import *
 from AE import *
 
+# One-Class SVM class functions
 class Decision():
     
     # One Class SVM parameter intialization
-    def __init__(self, nu, kernel, gamma):
+    # labels_test for accuracy
+    def __init__(self, nu, kernel, gamma, base_vec, labels_train, labels_test):
         self.nu = nu
         self.kernel = kernel
         self.gamma = gamma
+        self.base_vec = base_vec
+        self.labels_train = labels_train
+        self.labels_test = labels_test
     
+    def MSE(self):
+        X_train = []
+        for batch in batch_size:
+            
+    
+    # train the One-Class SVM for a specific signature
     def fit_model(self):
+        clf = svm.OneClassSVM(nu=self.nu, kernel=self.kernel, gamma=self.gamma)
+        X_train = self.MSE()
+        Y_train = self.labels
+        clf.fit(X_train, Y_train)
+        
+    def predict(self):
+        
         
