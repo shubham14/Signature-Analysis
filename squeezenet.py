1414365bccdfbe89d2fec30cab60b2e2e69d32f3
Shubham Dash <shubham.ddash@gmail.com>
2018-06-12 14:41:05 -0400

    Squeezenet for compact training model

---
+++ squeezenet.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 12 14:13:41 2018
+
+@author: Shubham
+"""
+
+import numpy as np
+import sys
+import os
+
+# import tensorflow libraries
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+
+# class instantiating Squeezenet model 
+class SqueezeNet():
+    
+    def __init__():
+        pass
+    
+    
